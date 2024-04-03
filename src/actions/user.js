63f95655d5035daabacae2b6dfc1e0ef588bee3f
Rahul@kumar <drkagarwal05@gmail.com>
2024-04-04 03:21:30 +0530

    issues fixed

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { userActions } from '../reducers/user';
-import { BASE_URL } from '../helper';
+import { BASE_URL, config } from '../helper';
 export const getUser = () => {
   return async (dispatch) => {
     const getUserData = async () => {
-      const response = await axios.get(`${BASE_URL}/api/user`);
+      const response = await axios.get(`${BASE_URL}/api/user`, config);
       if (response.statusText != 'OK') {
         throw new Error('Something went wrong');
       }
@@ -15,7 +15,7 @@ export const getUser = () => {
       dispatch(userActions.getUserRequest());
 
       const userData = await getUserData();
-      dispatch(userActions.getUserSuccess(userData));
+      dispatch(userActions.getUserSuccess(userData.user));
     } catch (err) {
       dispatch(userActions.getUserFailure(err.response.data.message));
     }
--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'http://localhost:4040';
 export const config = {
   headers: {
     'Content-Type': 'application/json',
